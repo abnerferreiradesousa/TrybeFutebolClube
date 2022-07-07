382b75a6dcd7e0d5342248ab691a544d3a703d61
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-07-07 09:24:51 -0300

    [feat]: requisito 01

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,37 @@
+import { STRING, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -2,7 +2,5 @@ import { App } from './app';
 import 'dotenv/config';
 
 const PORT = process.env.APP_PORT || 3001;
-console.log("Abner");
-
 
 new App().start(PORT);
