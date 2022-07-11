4f80a72bb153ae67e28d0b7d073a55e3f1496171
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-07-11 18:30:41 -0300

    [feat]: class TeamService implementada com dois métodos, getById e getAll

---
+++ app/backend/src/services/team.service.ts
@@ -0,0 +1,28 @@
+import { StatusCodes } from 'http-status-codes';
+import errorMessage from '../utils/error.message';
+import Team from '../database/models/users';
+
+export default class TeamService {
+  public model = Team;
+  constructor(model = Team) {
+    this.model = model;
+  }
+
+  public async getAll(): Promise<Team[]> {
+    const teamsData = await this.model.findAll();
+    // if (!userData) {
+    //   throw errorMessage(StatusCodes.UNAUTHORIZED, ');
+    // }
+    return teamsData;
+  }
+
+  // Como pegar os dados que estão dentro do userData?
+
+  public async getById(id: number): Promise<Team> {
+    const teamData = await this.model.findOne({ where: { id } });
+    if (!teamData) {
+      throw errorMessage(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
+    }
+    return teamData;
+  }
+}
